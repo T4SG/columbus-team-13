67a8b423acb32722fdeaef7152b8d9bbb2223f41
Richard Quackenbush <quackenbushrichard@gmail.com>
2015-10-03 03:09:07 -0400

    home page

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
     <div class="col-xs-2">
       <img src="logo.png" alt="" class="logo-img" id="logo"/>
     </div>
+
     <div class="col-xs-8">
       <ul class="nav nav-tabs">
         <li role="presentation"><a href="index.php">Home</a></li>
@@ -9,7 +10,11 @@
         <li role="presentation"><a href="stories.php">My Story</a></li>
       </ul>
     </div>
+
     <div class="col-xs-2 col-centered"> 
-        <h3><span class="label label-default" id="green">Hello, John Doe!</span></h3>
+       <?php if(!isset($_GLOBALS['do_not_render_name'])): ?>
+       	      <h3><span class="label label-default" id="green">Hello, John Doe!</span></h3>
+       <?php endif; ?>
     </div>
+
 </div>
