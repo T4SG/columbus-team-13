b80d851346c4867a54f150eab85a4ba6ec68279d
Richard Quackenbush <quackenbushrichard@gmail.com>
2015-10-03 05:43:17 -0400

    re-reverted references

--- References.php
+++ References.php
@@ -28,32 +28,13 @@
   <div class="col-xs-2"> </div>
   <div class="col-xs-8">
     <h2 class="italics-header">Resources</h2>
-    <h2 class="sample-user-name">because even Heroes can ask for help...</h2>
-    <h4>Here are some important resources that you can use at anytime:</h4>
     <ul>
         <li><a href="http://lebronjamesfamilyfoundation.org/"><h4>Lebron James Foundation <h4></a></li>
         <li><a href="http://www.suicidepreventionlifeline.org/"><h4>National Suicide Prevention Hotline: 1 (800) 273-8255<h4></a></li>
         <li><a href="http://www.teenhealthandwellness.com/static/hotlines"><h4>Other Hotlines<h4></a></li>
       </ul>
     <hr />
-
-    <!--<div class="container" id="profile">-->
-    <div class="row">
-      <div class="col-lg-6 col-lg-offset-1">
-      <h1 class="no-top-margin">because even Heroes can ask for help...</h1>
- 
- 
-  <!--</div>-->
-
-    <!--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>-->
-  </div>
-  <div class="col-xs-2">
-        <li><a href="#news"><h4>Success stories <h4></a></li>
-        <li><a href="#guide"><h4>College application guide<h4></a></li>
-        <li><a href="#inbox"><h4>Contact us<h4> </a></li>
-      </ul>
-
-  </div>
+    </div>
   <div class="col-xs-2"></div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
