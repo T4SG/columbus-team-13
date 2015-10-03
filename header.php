8145b6ac4ddfb7aec748a1566f75575c877435c7
Malika Shahrawat <malikas@mit.edu>
2015-10-03 01:33:30 -0400

    added label of user's name

--- header.php
+++ header.php
@@ -1,13 +1,17 @@
 <div class="col-xs-12 main-header">
     <div class="col-xs-2">
-      <img src="logo.png" alt="" class="logo-img" />
+      <img src="logo.png" alt="" class="logo-img" id="logo"/>
     </div>
     <div class="col-xs-8">
       <ul class="nav nav-tabs">
         <li role="presentation"><a href="#">Home</a></li>
         <li role="presentation" class="active"><a href="#">Profile</a></li>
         <li role="presentation"><a href="#">My Story</a></li>
+        <li role="presentation"><a href="#">Your Story</a></li>
+        <li role="presentation"><a href="#">Resources</a></li>
       </ul>
     </div>
-    <div class="col-xs-2"> </div>
+    <div class="col-xs-2 col-centered"> 
+        <h3><span class="label label-default" id="green">Hello, John Doe!</span></h3>
+    </div>
 </div>
