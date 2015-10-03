41bebbc0ab3eff1a228c404df12bdb950e3ef9af
unknown <brussell24@BRUSSELL24-2.cscfed.root.ad>
2015-10-03 02:18:58 -0400

    new tab

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
         <li role="presentation"><a href="index.php">Home</a></li>
         <li role="presentation" class="active"><a href="#">Profile</a></li>
         <li role="presentation"><a href="stories.php">My Story</a></li>
+        <li role="presentation"><a href="personal_page.php">My Progress</a></li>
       </ul>
     </div>
     <div class="col-xs-2 col-centered"> 
