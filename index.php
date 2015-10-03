afeb09cde368c17c86b002586ff895c1ee0a5e30
NebrasA <nalnemer5@gmail.com>
2015-10-03 02:36:50 -0400

    add graph to show student's grades over a period of time.

--- index.php
+++ index.php
@@ -54,32 +54,58 @@
 
   </div>
   <h3 id="centered" style="text-align:center">Academic Progress</h3>
-   <script>
-var text = {
-    "StudentGrades": [
-        {
-            "Exam": 67
-        },
-        {
-            "Exam": 53
-        },
-        {
-            "Exam": 42
-        },
-        {
-            "Exam": 78
-        },
-        {
-            "Exam": 85
-        },
-        {
-            "Exam": 98
-        }
-    ]
-	
-	var obj = JSON.parse(text);
-}
- </script >
+
+ 
+  
+  
+  <div>
+    <script type="text/javascript"
+          src="https://www.google.com/jsapi?autoload={
+            'modules':[{
+              'name':'visualization',
+              'version':'1',
+              'packages':['corechart']
+            }]
+          }"></script>
+
+    <script type="text/javascript">
+      google.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+		  var jsonData = $.ajax({
+          url: "getData.php",
+          dataType: "json",
+          async: false
+          }).responseText;
+		  
+		  
+		  
+       var data = new google.visualization.DataTable(jsonData);
+
+        var options = {
+          title: 'Student Performance',
+          curveType: 'function',
+		  pointSize: 20,
+          legend: { position: 'bottom' }
+			
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </div>
+  <div>
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
+  </div>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   <!--</div>-->
 
     <!--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>-->
