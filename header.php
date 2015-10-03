a90569da08e490a1cb7ca5063aa30fdce1fbd477
Richard Quackenbush <quackenbushrichard@gmail.com>
2015-10-03 01:45:48 -0400

    fixed the thing

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
     </div>
     <div class="col-xs-8">
       <ul class="nav nav-tabs">
-        <li role="presentation"><a href="#">Home</a></li>
+        <li role="presentation"><a href="index.php">Home</a></li>
         <li role="presentation" class="active"><a href="#">Profile</a></li>
-        <li role="presentation"><a href="#">My Story</a></li>
+        <li role="presentation"><a href="stories.php">My Story</a></li>
       </ul>
     </div>
     <div class="col-xs-2"> </div>
---
+++ stories.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>MyStory / YourStory</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php include_once('include-header.php'); ?>
+  </head>
+  <body>
+  <?php include_once('header.php'); ?>
+  <div class="col-xs-2"> </div>
+  <div class="col-xs-8">
+    <h2 class="italics-header">MyStory / YourStory</h2>
+    <hr />
+    <div class="my-story col-xs-6">
+      <img class="sample-user" src="images/mystory.png" alt= "" /> 
+    </div>
+    <div class="your-story col-xs-6">
+      <h2 class="sample-user-name">LeBron James</h2>
+      <img class="sample-user" src="images/lebron-james.jpg" />
+      <iframe width="100%" height="315px" src="https://www.youtube.com/embed/_mgBStqp06c" frameborder="0" allowfullscreen></iframe>
+      <h3 class="italics-header">My Goals</h3>
+      <ul class="goals">
+	<li>Score 20 points in one game</li>
+	<li>Five reachback videos in one week</li>
+	<li>Spending four nights a week with family</li>
+      </ul>
+    </div>
+  </div>
+  <div class="col-xs-2"></div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
