ada602b76efc81320dde1b8c709eff1e6d07ae94
Richard Quackenbush <quackenbushrichard@gmail.com>
2015-10-03 05:40:34 -0400

    Revert "update files"
    This reverts commit 45e9e8583a575718ae091dd8409c5ebaffef96ff, reversing
    changes made to 222b9efeb79c5f147b12daa6b73f9efc2ae1004b.

--- References.php
+++ References.php
@@ -5,7 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<<<<<<< HEAD
     <title>Resources</title>
+=======
+    <title>MyStory - YourStory</title>
+     
+
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="morecss.css" rel="stylesheet">
@@ -23,7 +28,6 @@
   <div class="col-xs-2"> </div>
   <div class="col-xs-8">
     <h2 class="italics-header">Resources</h2>
-<<<<<<< HEAD
     <h2 class="sample-user-name">because even Heroes can ask for help...</h2>
     <h4>Here are some important resources that you can use at anytime:</h4>
     <ul>
@@ -31,7 +35,6 @@
         <li><a href="http://www.suicidepreventionlifeline.org/"><h4>National Suicide Prevention Hotline: 1 (800) 273-8255<h4></a></li>
         <li><a href="http://www.teenhealthandwellness.com/static/hotlines"><h4>Other Hotlines<h4></a></li>
       </ul>
-=======
     <hr />
 
     <!--<div class="container" id="profile">-->
@@ -45,9 +48,13 @@
     <!--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>-->
   </div>
   <div class="col-xs-2">
->>>>>>> 2a0204d746415fd90721192c7cdbb805142cf63a
+        <li><a href="#news"><h4>Success stories <h4></a></li>
+        <li><a href="#guide"><h4>College application guide<h4></a></li>
+        <li><a href="#inbox"><h4>Contact us<h4> </a></li>
+      </ul>
 
   </div>
+  <div class="col-xs-2"></div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
