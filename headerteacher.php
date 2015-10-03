b4a200bce818add4201fb1738e756f5971006f05
Malika Shahrawat <malikas@mit.edu>
2015-10-03 04:03:11 -0400

    fixed links on header

--- headerteacher.php
+++ headerteacher.php
@@ -2,16 +2,22 @@
     <div class="col-xs-2">
       <img src="logo.png" alt="" class="logo-img" id="logo"/>
     </div>
+
     <div class="col-xs-8">
       <ul class="nav nav-tabs">
-        <li role="presentation"><a href="#">Home</a></li>
-        <li role="presentation" class="active"><a href="#">Profile</a></li>
-        <li role="presentation"><a href="#">My Story</a></li>
-        <li role="presentation"><a href="#">Your Story</a></li>
-        <li role="presentation"><a href="#">Resources</a></li>
+        <li role="presentation"><a href="index.php">Home</a></li>
+        <li role="presentation" class="active"><a href="profile.php">Profile</a></li>
+        <li role="presentation"><a href="stories.php">My Story</a></li>
+        <li role="presentation"><a href="personal_page.php">My Progress</a></li>
+        <li role="presentation"><a href="References.php">Resources</a></li>
+        <li role="presentation"><a href="teacher.php">Teacher Page</a></li>
       </ul>
     </div>
+
     <div class="col-xs-2 col-centered"> 
-        <h3><span class="label label-default" id="green">Hello, Ms. Smith!</span></h3>
+       <?php if(!isset($_GLOBALS['do_not_render_name'])): ?>
+              <h3><span class="label label-default" id="green">Hello, Ms. Smith!</span></h3>
+       <?php endif; ?>
     </div>
+
 </div>
