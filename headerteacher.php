f1b37d0067c11d276a6d7058f2610bbad4de7eb2
Malika Shahrawat <malikas@mit.edu>
2015-10-03 02:47:32 -0400

    header of teacher page

---
+++ headerteacher.php
@@ -0,0 +1,17 @@
+<div class="col-xs-12 main-header">
+    <div class="col-xs-2">
+      <img src="logo.png" alt="" class="logo-img" id="logo"/>
+    </div>
+    <div class="col-xs-8">
+      <ul class="nav nav-tabs">
+        <li role="presentation"><a href="#">Home</a></li>
+        <li role="presentation" class="active"><a href="#">Profile</a></li>
+        <li role="presentation"><a href="#">My Story</a></li>
+        <li role="presentation"><a href="#">Your Story</a></li>
+        <li role="presentation"><a href="#">Resources</a></li>
+      </ul>
+    </div>
+    <div class="col-xs-2 col-centered"> 
+        <h3><span class="label label-default" id="green">Hello, Ms. Smith!</span></h3>
+    </div>
+</div>
